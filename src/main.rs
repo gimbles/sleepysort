3d9679b33be596713a3ef3a84c1fc496f2cf9d41
Yush08 <yusharora@protonmail.com>
2021-12-13 21:56:25 +0530

    add: Code

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use tokio::task::spawn_blocking;
+use std::{thread, time};
+
+#[tokio::main]
+async fn main() {
+	let unsorted = [6,7,3,5,1,7,9];
+	
+	for i in unsorted {
+		spawn_blocking(move || {
+			let mils = time::Duration::from_millis(i*10);
+			thread::sleep(mils);
+			println!("{}", i);
+		});
+	}
+	
+}
