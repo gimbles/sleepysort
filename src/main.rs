bcb293b20626d69119304cadd7e2818f3bdd5e55
Yush08 <yusharora@protonmail.com>
2021-12-14 11:12:07 +0530

    fmt: Apply RustFmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use tokio::task::spawn_blocking;
 use std::{thread, time};
+use tokio::task::spawn_blocking;
 
 #[tokio::main]
 async fn main() {
-	let unsorted = [6,7,3,5,1,7,9];
-	
-	for i in unsorted {
-		spawn_blocking(move || {
-			let mils = time::Duration::from_millis(i*10);
-			thread::sleep(mils);
-			println!("{}", i);
-		});
-	}
-	
+    let unsorted = [6, 7, 3, 5, 1, 7, 9];
+
+    for i in unsorted {
+        spawn_blocking(move || {
+            let mils = time::Duration::from_millis(i * 10);
+            thread::sleep(mils);
+            println!("{}", i);
+        });
+    }
 }
